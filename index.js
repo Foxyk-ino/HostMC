1822a644ab5a1285940354c49575b6bcea69265f
Foxyk-ino <kirileremejcuk@gmail.com>
2025-04-06 13:48:04 +0300

    index.js

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ bot.on('chat', (username, message) => {
     bot.chat('Привет, ' + username + '!')
   }
 })
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Привет, мир!');
+});
+
+app.listen(port, () => {
+  console.log(`Сервер работает на порту ${port}`);
+});
